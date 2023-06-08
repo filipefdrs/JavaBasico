5edceb95114b194d248d3b7dd041e2b637d8d2fd
Filipe Reis <filipedosreissantos@hotmail.com>
2023-06-08 19:36:04 -0300

    Digital Innovation One - Formação Java Developer
    Signed-off-by: Filipe Reis <filipedosreissantos@hotmail.com>

--- src/edu/filipe/primeirasemana/AnatomiaDasClasses2.java
+++ src/edu/filipe/primeirasemana/AnatomiaDasClasses2.java
@@ -2,10 +2,10 @@ package edu.filipe.primeirasemana;
 
 public class AnatomiaDasClasses2 {
     public static void main(String[] args) {
-       int ano = 2021;
-       ano = 2022;
+       //int ano = 2021;
+      // ano = 2022;
 
-       final String BR = "Brasil";
+       //final String BR = "Brasil";
        //BR = "Brazil";
     }
 }
--- src/edu/filipe/primeirasemana/BoletimEstudantil.java
+++ src/edu/filipe/primeirasemana/BoletimEstudantil.java
@@ -1,8 +1,10 @@
 package edu.filipe.primeirasemana;
+import java.util.Scanner;
 
 public class BoletimEstudantil {
     public static void main(String[] args) {
-        int mediaFinal = 7;
+        Scanner scanner = new Scanner(System.in);
+        int mediaFinal = scanner.nextInt();
 
         if(mediaFinal < 6) {
             System.out.println("REPROVADO");
